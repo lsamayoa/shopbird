0764bde825ac4962f53b9492647dc382b386215b
Leonel Samayoa <leonel.samayoa@xoom.com>
2016-07-19 18:23:40 -0600

    patching env

--- config/prod.exs
+++ config/prod.exs
@@ -14,9 +14,7 @@ use Mix.Config
 config :shopbird, Shopbird.Endpoint,
   http: [port: {:system, "PORT"}],
   #url: [host: "example.com", port: 80],
-  url: [scheme: "https",
-    host: System.get_env("PUBLIC_HOST") || "shopbird.herokuapp.com",
-    port: 443],
+  url: [scheme: "https", host: System.get_env("PUBLIC_HOST") || "shopbird.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
