6d175a291a722cbbb58b67f9119ac60ec2ab3c48
Leonel Samayoa <leonel.e.samayoa@gmail.com>
2016-10-25 19:39:18 -0600

    Upgraded elixir version to 1.3.4 and erlang to 19.1 for sentry support

--- config/config.exs
+++ config/config.exs
@@ -40,6 +40,11 @@ config :ueberauth, Ueberauth,
     ] },
   ]
 
+config :sentry, dsn: System.get_env("SENTRY_DSN"),
+   included_environments: ~w(production staging),
+   error_logger: true,
+   environment_name: System.get_env("DEPLOY_ENV") || "development"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
